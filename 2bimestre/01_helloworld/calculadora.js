94bdf6e1e9fe1466f439669c8d7e39fd06fc8172
Pedro33Carneiro <124801768+Pedro33Carneiro@users.noreply.github.com>
2024-06-20 08:16:19 -0300

    Delete 2bimestre/01_helloworld directory

--- 2bimestre/01_helloworld/calculadora.js
+++
@@ -1,9 +0,0 @@
-var SomarFunc = require("./modulos/somar")
-var SubtrairFunc = require("./modulos/subtrair")
-var MultiplicarFunc = require("./modulos/multiplicar")
-var DividirFunc = require("./modulos/dividir")
-
-console.log (SomarFunc(5,5))
-console.log (SubtrairFunc(5,5))
-console.log (MultiplicarFunc(5,5))
-console.log (DividirFunc(5,5))
